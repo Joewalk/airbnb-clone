a7f5dc218f2a737991b137756bd85c2eb08b9997
Joewalk <joenart2@gmail.com>
2020-10-05 15:36:58 +0100

    Initialize project using Create React App

---
+++ firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAT2Acw578LlOHhf3vBiYYpQ_E9um1kA5c",
+  authDomain: "airbnb-clone-6cca8.firebaseapp.com",
+  databaseURL: "https://airbnb-clone-6cca8.firebaseio.com",
+  projectId: "airbnb-clone-6cca8",
+  storageBucket: "airbnb-clone-6cca8.appspot.com",
+  messagingSenderId: "763664304617",
+  appId: "1:763664304617:web:15dcbe8a9e8452d034e378",
+};
