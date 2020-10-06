6edb3283d96e2e3ef1efc2eea8e0256e6e79aec2
Joewalk <joenart2@gmail.com>
2020-10-06 18:31:42 +0100

    styled the head part of the search page0

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Button } from "@material-ui/core";
+import "./SearchPage.css";
 
 function SearchPage() {
   return (
@@ -6,6 +8,11 @@ function SearchPage() {
       <div className="searchPage__info">
         <p>62 stays . 26 august to 30 august 2 guests</p>
         <h1>stays nearby</h1>
+        <Button variant="outlined">Cancellation Flexibility</Button>
+        <Button variant="outlined">Type of place</Button>
+        <Button variant="outlined">Price</Button>
+        <Button variant="outlined">Rooms and Beds</Button>
+        <Button variant="outlined">More filters</Button>
       </div>
     </div>
   );
